600026d230f2e695693c0edecc63267e1dac9053
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-12-10 13:16:26 +0100

    Add project files.

---
+++ IdentityDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using IdentityDemo.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<AccountService>();
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
